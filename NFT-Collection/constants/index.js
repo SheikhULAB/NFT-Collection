8c7d46024f3c74b58b4bc9f62fe78ce2ad4edef9
SheikhULAB <tosibalaptop86@gmail.com>
2022-05-14 17:54:32 +0600

    first commit

---
+++ NFT-Collection/constants/index.js
@@ -0,0 +1,9 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x628e7c2Ed6A54dB4A5685Bc219699ed4490A3800";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+
+// 0xca850Ad660C09b7FeCC37FD91844479F39106B02
